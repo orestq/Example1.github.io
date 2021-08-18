b280a059191bb5980d6a2b90da8b20f613a41992
Orest Lentsyk <oresttrue@gmail.com>
2021-08-18 17:46:37 +0300

    change class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+$(document).ready(function () {
+    $(".card-item-btn").click(function () {
+        $(this).parent(".card-item").children(".popup").addClass("show-modal");
+    });
+
+    $(".close, .popup-overlay").click(function () {
+        $(".popup").removeClass("show-modal");
+    })
+
+    $(".mobile-menu").click(function () {
+        $(this).parent(".menus").children(".header-nav-list").toggleClass("header-nav-list, show");
+        $(this).parent(".menus").children(".mobile-menu").toggleClass("active-menu");
+    });
+});
+
+
 // let mobileIcon = document.querySelector('.mobile-menu');
 // let menu = document.querySelector('.header-nav-list');
 
@@ -28,34 +44,23 @@
 //     $(".mobile-menu").toggleClass("active-menu");
 // }
 
-$(document).ready(function () {
-    $(".card-item-btn").click(function () {
-        $(this).parent(".card-item").children(".popup").addClass("show-modal");
-    });
 
-    $(".close, .popup-overlay").click(function () {
-        $(".popup").removeClass("show-modal");
-    })
+// **********************************************************************************
 
-    $(".mobile-menu").click(function () {
-        $(this).parent(".menus").children(".header-nav-list").toggleClass("header-nav-list, show");
-        $(this).parent(".menus").children(".mobile-menu").toggleClass("active-menu");
-    });
-});
 
-    // $(".card-item-btn").click(function () {
-    //     if ($(this).attr("data-btn") == '1') {
-    //         $(".popup-start").toggleClass("show-modal");
-    //     } else if ($(this).attr("data-btn") == '2') {
-    //         $(".popup-business").toggleClass("show-modal")
-    //     } else if ($(this).attr("data-btn") == '3') {
-    //         $(".popup-professional").toggleClass("show-modal");
-    //     }
-    // });
+// $(".card-item-btn").click(function () {
+//     if ($(this).attr("data-btn") == '1') {
+//         $(".popup-start").toggleClass("show-modal");
+//     } else if ($(this).attr("data-btn") == '2') {
+//         $(".popup-business").toggleClass("show-modal")
+//     } else if ($(this).attr("data-btn") == '3') {
+//         $(".popup-professional").toggleClass("show-modal");
+//     }
+// });
 
-    // $(".close, .popup-overlay").click(function () {
-    //     $(".popup-start").removeClass("show-modal");
-    //     $(".popup-business").removeClass("show-modal");
-    //     $(".popup-professional").removeClass("show-modal");
-    // })
+// $(".close, .popup-overlay").click(function () {
+//     $(".popup-start").removeClass("show-modal");
+//     $(".popup-business").removeClass("show-modal");
+//     $(".popup-professional").removeClass("show-modal");
+// })
 
